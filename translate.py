072f4d25fe35ae6d995f203194f60fccba52103e
Beatriz Motta <62359366+beatrizmotta@users.noreply.github.com>
2022-11-19 20:24:31 -0300

    Delete translate.py

--- translate.py
+++
@@ -1,9 +0,0 @@
-from googletrans import Translator, LANGUAGES
-
-translator = Translator()
-
-for language in LANGUAGES:
-    dest = translator.translate("eu, você nossos", dest=language).text
-    print(dest)
-# dest = translator.translate("And that's the way i loved you", dest="pt")
-# print(LANGUAGES)
