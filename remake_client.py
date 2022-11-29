5e8b6daea35a033624da2c2bca7472c19e24b1c9
Beatriz Motta <bigobaldohoran@gmail.com>
2022-11-28 22:49:46 -0300

    /Adição de alguns comentários explicando o código /Error handling na escolha da linguagem /Tentativa de tentar encerrar corretamente as conexões com o '/quit'

--- remake_client.py
+++ remake_client.py
@@ -2,6 +2,12 @@ import socket
 import threading
 from googletrans import LANGUAGES, Translator
 
+def language_validator(language_code):
+    while language_code not in LANGUAGES.keys():
+        print("Language code not valid, please type another:")
+        language_code = input("> ")
+    return language_code 
+
 HOST = 'localhost'
 PORT = 5050
 
@@ -12,10 +18,15 @@ for i in LANGUAGES:
     print(f'- {LANGUAGES[i]} [{i}]')
 print("Choose a language: \nExample: 'en', 'pt', 'es'")
 language_choosen = input("> ")
+language_choosen = language_validator(language_choosen)
 
 nickname_prompt = translator.translate("Qual é o seu apelido?", src='pt', dest=language_choosen).text
 room_closed_warning = translator.translate("A sala do chat foi encerrada.", src='pt', dest=language_choosen).text
 me_prompt = translator.translate("eu, você nossos", src='pt', dest=language_choosen).text.split(",")[0]
+
+room_open = True
+
+
 
 nickname = input(nickname_prompt)
 
@@ -27,6 +38,9 @@ client.send(f'nick={nickname}\lang={language_choosen}'.encode())
 def receive():
     while True:
         try:
+            # A Receive vai tentar receber algo do outro lado do código. 
+            # Caso ele consiga, ele vai separar a mensagem em mensagem e linguagem.
+            #
             data = client.recv(1024).decode()
             language = data.split("\\")[0].split("=")[1]
             msg = data.split("\\")[1].split("=")[1]
@@ -34,14 +48,21 @@ def receive():
             translated_msg = translator.translate(msg, src=language, dest=language_choosen).text
             print(translated_msg)
         except:
+            # Caso não consiga receber, ele deve fechar a conexão também
             print(room_closed_warning)
+            room_open = False
             client.close()
             break
+    return
         
 def send():
     while True:
+        if not room_open:
+            print("Eita fechou")
+            break
         msg = input(f'{me_prompt}:')
         client.send(f'{nickname}: {msg}'.encode())
+    return
 
 receive_thread = threading.Thread(target=receive)
 send_thread = threading.Thread(target=send)
--- remake_server.py
+++ remake_server.py
@@ -2,11 +2,12 @@ import socket
 import logging
 import threading 
 from googletrans import Translator
-
 #####     SCRIPT DE CHAT     #####
 logging.basicConfig(level=logging.INFO)
 logging.basicConfig(level=logging.ERROR)
 clients = dict()
+
+should_work = True
 
 HOST = 'localhost'
 PORT = 5050 
@@ -21,7 +22,17 @@ def control():
     while True:
         control = input(">> ")
         if (control == '/quit'):
+            print("Estou teoricamente fechando")
+            global should_work
+            should_work = False
+
+            for client in clients:
+                clients[client]["connection"].close()
+
+            server.shutdown(socket.SHUT_RDWR)
             server.close()
+            break
+    return 
 
 def transmitir(msg, address_code):
     decoded_msg = msg.decode()
@@ -34,6 +45,13 @@ def transmitir(msg, address_code):
 
 def handle_client(client, address_code):
     while True:
+        print("Handling client")
+        # Handle Client vai tentar receber algo do outro lado do socket.
+        # Caso ele consiga, ele faz o decode da mensagem recebida e lida com ela,
+        # fazendo o 'cadastro' do emissor caso seja a primeira vez que ele estabelece
+        # o contato com o servidor (a primeira mensagem enviada pelo cliente
+        # sempre vai conter apenas o seu nome de usuário e linguagem) ou transmitindo
+        # a mensagem mandada caso contrário. 
         try:
             data = client.recv(1024)
             decoded_data = data.decode()
@@ -46,31 +64,38 @@ def handle_client(client, address_code):
                 clients[address_code]['language'] = language  
             else:
                 transmitir(data, address_code)
-
-        except:
+        
+        # Caso não consiga receber a mensagem (ocorreu algum erro na função recv) então
+        # a conexão com tal client deve ser fechada e ele deve ser retirado da lista de 
+        # usuários ativos. 
+        except: 
             alias = clients[address_code]['nick']
             clients[address_code]['connection'].close()
             del clients[address_code]
             transmitir(f"{alias} saiu do chat", address_code)
             break
+    return 
         
 
-control_thread = threading.Thread(target=control)
-control_thread.start()
-
-while True:
+def main():
     
-    client, address = server.accept()
-    logging.info(f'{address} was connected')
+    control_thread = threading.Thread(target=control)
+    control_thread.start()
 
-    print(type(address))
-    address = "".join([str(i) for i in list(address)])
+    while True:
+        
+        client, address = server.accept()
+        logging.info(f'{address} was connected')
 
-    clients[address] = {
-        'connection': client,
-        'nick': ''
-    }
+        print(type(address))
+        address = "".join([str(i) for i in list(address)])
 
-    thread = threading.Thread(target=handle_client, args=(client, address))
-    thread.start()
-    
+        clients[address] = {
+            'connection': client,
+            'nick': ''
+        }
+
+        thread = threading.Thread(target=handle_client, args=(client, address))
+        thread.start()
+
+main()
