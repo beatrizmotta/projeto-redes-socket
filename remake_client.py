a9911854f10fdd033c1fcf60521fa61630bd0d06
SarahDomingos <92494941+sarahdomingos@users.noreply.github.com>
2022-12-04 20:25:26 -0300

    added comments

--- remake_client.py
+++ remake_client.py
@@ -1,10 +1,14 @@
-import socket 
-import threading
-from googletrans import LANGUAGES, Translator
+# Comando de execução do programa pelo terminal: py remake_client.py
 
+import socket # Biblioteca padrão.
+import threading # Biblioteca padrão.
+from googletrans import LANGUAGES, Translator # pip install googletrans==4.0.0-rc1
+
+# Definindo endereço IP e de porta do servidor.
 HOST = 'localhost'
 PORT = 5050
 
+# Checagem para input do idioma escolhido pelo usuário.
 def language_validator(language_code):
     while language_code not in LANGUAGES.keys():
         print("Language code not valid, please type another:")
@@ -13,48 +17,55 @@ def language_validator(language_code):
 
 translator = Translator()
 
+# Exibe os idiomas disponíveis ao usuário.
 print("Choose a language:")
 for i in LANGUAGES:
     print(f'- {LANGUAGES[i]} [{i}]')
 
+# Input do idioma.
 print("Choose a language: \nExample: 'en', 'pt', 'es'")
 language_choosen = input("> ")
 language_choosen = language_validator(language_choosen)
 
+# Input do nome do usuário.
 nickname_prompt = translator.translate("Qual é o seu apelido?", src='pt', dest=language_choosen).text
+# Tradução da mensagem exibida quando o servidor é encerrado.
 room_closed_warning = translator.translate("A sala do chat foi encerrada.", src='pt', dest=language_choosen).text
-# me_prompt = translator.translate("eu, você nossos", src='pt', dest=language_choosen).text.split(",")[0]
 
 nickname = input(nickname_prompt + ' ')
 
+# Criando socket no formato IPv4 (INET - requisita host e port) e com conexão TCP (STREAM).
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# Conectando cliente ao servidor na porta "PORT".
 client.connect((HOST, PORT))
-
+# Envia ao servidor as informações do usuário.
 client.send(f'nick={nickname}\lang={language_choosen}'.encode())
 
 is_server_up = True
 
+# Função de recepção dos dados.
 def receive():
     while True:
         try:
+            # Recebe e decodifica os dados transmitidos pelo servidor com no máximo 1024 bits.
             data = client.recv(1024).decode()
+            # Tratamento dos dados.
             language = data.split("\\")[0].split("=")[1]
             msg = data.split("\\")[1].split("=")[1]
-
+            # Traduz a mensagem recebida para o idioma do usuário.
             translated_msg = translator.translate(msg, src=language, dest=language_choosen).text
-            # print ("\033[A                             \033[A")
             print(translated_msg)
 
-        except Exception as e:
-            #print(e)
+        except:
+            # Exceção para quando o servidor é encerrado.
             print(room_closed_warning)
             client.close()
             global is_server_up
             is_server_up = False
             break
-    # print("RECEIVE -- reached the end of the loop")
     return 
-        
+
+# Função de envio dos dados para o servidor.       
 def send():
     while True:
         msg = input()
@@ -64,9 +75,9 @@ def send():
         else:
             break
 
-
+# Cria uma thread de recepção e envio.
 receive_thread = threading.Thread(target=receive)
 send_thread = threading.Thread(target=send)
-
+# Inicialização da thread.
 receive_thread.start()
 send_thread.start()
--- remake_server.py
+++ remake_server.py
@@ -1,28 +1,37 @@
-import socket
-import logging
-import threading 
-from googletrans import Translator
+# Comando de execução do programa pelo terminal: py remake_server.py
+
+import socket # Biblioteca padrão.
+import logging # Biblioteca padrão.
+import threading # Biblioteca padrão.
+from googletrans import Translator # pip install googletrans==4.0.0-rc1
 
 #####     SCRIPT DE CHAT     #####
 logging.basicConfig(level=logging.INFO)
 logging.basicConfig(level=logging.ERROR)
 clients = dict()
 
+# Definindo endereço IP e de porta do servidor.
 HOST = 'localhost'
 PORT = 5050 
+# Criando socket no formato IPv4 (INET - requisita host e port) e com conexão TCP (STREAM).
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# Servidor visível apenas dentro do dispositivo nos endereços de HOST e PORT.
 server.bind((HOST, PORT))
+
 translator = Translator()
 
+# Inicialização do modo de escuta do servidor.
 server.listen() 
 logging.info("Servidor rodando")
 
+# Função de controle para encerrar o servidor.
 def control():
     while True:
         control = input(">> ")
         if (control == '/quit'):
             server.close()
 
+# Função de transmissão para todos os clientes conectados ao servidor (envio da mensagem, nome e idioma do remetente).
 def transmitir(msg, address_code):
     decoded_msg = msg.decode()
     nickname_transmissor = decoded_msg.split(":")[0]
@@ -32,6 +41,7 @@ def transmitir(msg, address_code):
         if (clients[client]['nick'] != nickname_transmissor):
             clients[client]['connection'].send(f'lang={language}\\msg={decoded_msg}'.encode())
 
+# Função de recepção dos dados com no máximo 1024 bits.
 def handle_client(client, address_code):
     while True:
         try:
@@ -46,7 +56,7 @@ def handle_client(client, address_code):
                 clients[address_code]['language'] = language  
             else:
                 transmitir(data, address_code)
-
+        # Exceção para quando um cliente se desconecta do servidor.
         except:
             alias = clients[address_code]['nick']
             clients[address_code]['connection'].close()
@@ -55,23 +65,25 @@ def handle_client(client, address_code):
                 clients[client1]['connection'].send(f"lang=pt\\msg={alias} saiu da sala".encode())
             break
         
-
+# Criação e inicialização da thread de controle.
 control_thread = threading.Thread(target=control)
 control_thread.start()
 
 while True:
-    
+    # Recebe os dados do cliente, quando o servidor aceita a conexão.
     client, address = server.accept()
     logging.info(f'{address} was connected')
 
     print(type(address))
     address = "".join([str(i) for i in list(address)])
 
+    # Adiciona o novo cliente ao dict() com seu respectivo endereço IP.
     clients[address] = {
         'connection': client,
         'nick': ''
     }
 
+    # Criação e inicialização da thread de controle de clientes.
     thread = threading.Thread(target=handle_client, args=(client, address))
     thread.start()
     
